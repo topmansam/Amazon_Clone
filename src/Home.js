256d6642ecb0c96611a4f8fbf13eb5c434adaec5
Sam <sam@Sams-MacBook-Air.local>
2023-07-27 17:38:40 -0400

    adding props to homepage

--- src/Home.js
+++ src/Home.js
@@ -11,12 +11,18 @@ function Home() {
         alt = ""
         />
          <div className= "home__row">
+         <Product title = "The lean startup" price = {29.99} image ="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg" rating ={5}/>
+        
             </div>
-                <Product />
+                
             <div className= "home__row">
+            <Product id = "12113" title = "Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater DOugh Hook and Whisk 5 Litre Glass Bowl"
+            price = {239.0} rating = {4} image ="https://images-na.ssl-images-amazon.com/images/I/810%2BGNdkzKL._AC_SX450_.jpg"/>
+          
             </div>
 
             <div className= "home__row">
+          
             </div>
       </div>
     </div>
--- src/Product.js
+++ src/Product.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import "./Product.css";
 
-function Product() {
-  return ( <div className= "product">
+function Product({id,title, image, price, rating}) {
+  return ( 
+  <div className= "product">
     <div className="product__info">
-        <p> The lean startup</p>
+        <p> {title}</p>
         <p className= "product__price">
             <small>$</small>
-            <strong> 19.99</strong>
+            <strong> {price}</strong>
         </p>
         <div className="product__rating">
-            <p>⭐</p>
+            {Array(rating)
+            .fill()
+            .map((_, i) =>(
+                <p>⭐</p>
+            ))}
+            
         </div>
     </div>
+     
+    <img src = {image} alt = ""/>
+    {/* <img></img> */}
+    <button>Add to Basket</button>
   </div>
   );
 }
