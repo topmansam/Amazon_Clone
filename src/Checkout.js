b71352add862b21f8b29b01c4479103f88991e56
Samuel <samuelonabolu@gmail.com>
2023-08-21 08:53:06 -0400

    deployed app

--- src/Checkout.js
+++ src/Checkout.js
@@ -5,7 +5,7 @@ import { useStateValue } from './StateProvider';
 import CheckoutProduct from './CheckoutProduct';
 
 function Checkout(){
-    const[{basket}, dispatch] = useStateValue();
+    const[{basket, user}, dispatch] = useStateValue();
     console.log("Current items in the basket:", basket);
     return (
     <div  className = "checkout">
@@ -15,6 +15,7 @@ function Checkout(){
         alt = ""
         />
         <div>
+            <h3> Hello, {user?.email}</h3>
             <h2
                 className = "checkout__title">Your shopping Basket
             </h2>
